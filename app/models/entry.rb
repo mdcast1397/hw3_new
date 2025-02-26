363f24a848ded309e323568b6d3051978a3f4b49
Michael  Castaneda <michael.castaneda@kellogg.northwestern.edu>
2025-02-26 22:47:47 +0000

    Completed Travel Log App

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ApplicationRecord
-  # talks to the "entries" table
+  belongs_to :place
+  validates :title, :description, :date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
-end
+    root "places#index"
+  
+    resources :places, only: [:index, :show, :new, :create] do
+      resources :entries, only: [:new, :create]
+    end
+  end
